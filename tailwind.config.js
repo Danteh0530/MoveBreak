bd5e783cd795c6757fb74107bcfbcb08fab468bc
Danteh0530 <mega83.ramirezjulian@gmail.com>
2025-05-19 20:26:08 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class',
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+        './resources/**/*.php',
+        './resources/**/*.html',
+        './resources/**/*.jsx',
+        './resources/**/*.tsx',
+        './resources/**/*.ts',
+        './resources/**/*.scss',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
